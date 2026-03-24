5bc1b43292b95196425df3e252bb51b7affa68c1
OTLICHNIKx <Kartoshka2281A@gmail.com>
2026-03-25 01:54:22 +0300

    fix container

--- backend/infrastructure/container.js
+++ backend/infrastructure/container.js
@@ -6,10 +6,11 @@
 // === РЕПОЗИТОРИИ ===
 const MongoTrackRepository = require('../repositories/mongo/MongoTrackRepository');
 const MongoTagRepository   = require('../repositories/mongo/MongoTagRepository');
-
+const MongoRepostRepository = require('../repositories/mongo/MongoRepostRepository');
 // === СЕРВИСЫ ===
 const LocalFileStorage = require('./services/LocalFileStorage');
 const MockAudioService = require('./services/MockAudioService');
+const MockNotificationService = require('./services/MockNotificationService');
 
 // === USE CASES ===
 const makeCreateTrack         = require('../usecases/createTrack');
@@ -21,12 +22,16 @@ const makeGetTrack            = require('../usecases/getTrack');
 const makeUpdateTrackMetadata = require('../usecases/updateTrackMetadata');
 const makeGetPopularTags      = require('../usecases/getPopularTags');
 const makeUploadAudio         = require('../usecases/uploadAudio');
+const makeRepostTrack          = require('../usecases/RepostTrack');
+const makeGetArtist           = require('../usecases/getArtist');
 
 // Создаём реализации
-const trackRepository = new MongoTrackRepository();
-const tagRepository   = new MongoTagRepository();
-const fileStorage     = new LocalFileStorage();
-const audioService    = new MockAudioService();
+const trackRepository     = new MongoTrackRepository();
+const tagRepository       = new MongoTagRepository();
+const fileStorage         = new LocalFileStorage();
+const audioService        = new MockAudioService();
+const repostRepository    = new MongoRepostRepository();
+const notificationService = new MockNotificationService();
 
 // Собираем use cases
 const container = {
@@ -39,6 +44,8 @@ const container = {
   updateTrackMetadata: makeUpdateTrackMetadata({ trackRepository }),
   getPopularTags:      makeGetPopularTags({ tagRepository }),
   uploadAudio:         makeUploadAudio({ trackRepository, fileStorage }),
+  RepostTrack:          makeRepostTrack({ trackRepository, repostRepository, notificationService }),
+  getArtist:           makeGetArtist({ trackRepository }),
 };
 
 module.exports = container;
