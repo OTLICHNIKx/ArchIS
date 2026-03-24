86573f4c7fa6af9635bde4a049866871b426e536
OTLICHNIKx <Kartoshka2281A@gmail.com>
2026-03-25 01:18:31 +0300

    fix container

--- backend/infrastructure/services/container.js
+++ backend/infrastructure/services/container.js
@@ -7,7 +7,9 @@ const MongoTagRepository   = require('../../repositories/mongo/MongoTagRepositor
 const InMemoryRepostRepository = require('../../repositories/mongo/MongoRepostRepository');
 
 // === СЕРВИСЫ ===
-const MockNotificationService = require('./MockNotificationService');
+const LocalFileStorage        = require('./services/LocalFileStorage');
+const MockAudioService        = require('./services/MockAudioService');
+const MockNotificationService = require('./services/MockNotificationService');
 
 // === USE CASES ===
 const makeCreateTrack         = require('../../usecases/createTrack');
@@ -19,35 +21,30 @@ const makeGetTrack            = require('../../usecases/getTrack');
 const makeUpdateTrackMetadata = require('../../usecases/updateTrackMetadata');
 const makeGetPopularTags      = require('../../usecases/getPopularTags');
 const makeUploadAudio         = require('../../usecases/uploadAudio');
-
 const makeRepostSong          = require('../../usecases/repostSong');
 const makeGetArtist           = require('../../usecases/getArtist');
 
 // Создаём реализации
-const trackRepository = new MongoTrackRepository();
-const tagRepository   = new MongoTagRepository();
-const repostRepository = new MongoRepostRepository();
+const trackRepository     = new MongoTrackRepository();
+const tagRepository       = new MongoTagRepository();
+const fileStorage         = new LocalFileStorage();
+const audioService        = new MockAudioService();
+const repostRepository    = new MongoRepostRepository();
 const notificationService = new MockNotificationService();
 
 // Собираем use cases
 const container = {
   createTrack:         makeCreateTrack({ trackRepository, tagRepository }),
-  publishTrack:        makePublishTrack({ trackRepository, audioService: null }),
+  publishTrack:        makePublishTrack({ trackRepository, audioService }),      // ← audioService не null!
   archiveTrack:        makeArchiveTrack({ trackRepository }),
-  deleteTrack:         makeDeleteTrack({ trackRepository, fileStorage: null }),
+  deleteTrack:         makeDeleteTrack({ trackRepository, fileStorage }),        // ← fileStorage не null!
   getArtistTracks:     makeGetArtistTracks({ trackRepository }),
   getTrack:            makeGetTrack({ trackRepository }),
   updateTrackMetadata: makeUpdateTrackMetadata({ trackRepository }),
   getPopularTags:      makeGetPopularTags({ tagRepository }),
-  uploadAudio:         makeUploadAudio({ trackRepository, fileStorage: null }),
-
-  // Новые use cases для воркфлоу репоста
-  repostSong: makeRepostSong({
-    trackRepository,
-    repostRepository,
-    notificationService
-  }),
-  getArtist: makeGetArtist({ trackRepository })
+  uploadAudio:         makeUploadAudio({ trackRepository, fileStorage }),        // ← fileStorage не null!
+  repostSong:          makeRepostSong({ trackRepository, repostRepository, notificationService }),
+  getArtist:           makeGetArtist({ trackRepository }),
 };
 
 module.exports = container;
